d5a474a3758bc82fa37ddea4dafa3c7f5b41554f
J. B. Rainsberger <me@jbrains.ca>
2015-05-17 16:26:48 +0200

    [FAILING TESTS] We can now run tests.

---
+++ test/source/ca/jbrains/pos/IsThisThingOn.java
@@ -0,0 +1,11 @@
+package ca.jbrains.pos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsThisThingOn {
+    @Test
+    public void hello() throws Exception {
+        Assert.assertTrue(false);
+    }
+}
