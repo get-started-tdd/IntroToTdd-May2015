a33c75b7cf522330bb3b0834a853147eff969ca1
J. B. Rainsberger <me@jbrains.ca>
2015-05-18 14:09:12 +0200

    Fractions can now remember their value, as long as that's an integer.

--- test/source/ca/jbrains/math/test/AddFractionsTest.java
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -1,6 +1,7 @@
 package ca.jbrains.math.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AddFractionsTest {
@@ -10,16 +11,27 @@ public class AddFractionsTest {
         Assert.assertEquals(0, sum.intValue());
     }
 
+    @Test
+    @Ignore
+    public void addendNotZero() throws Exception {
+        Fraction sum = new Fraction(1).plus(new Fraction(0));
+        Assert.assertEquals(1, sum.intValue());
+
+    }
+
     public static class Fraction {
+        private int integerValue;
+
         public Fraction(int integerValue) {
+            this.integerValue = integerValue;
         }
 
         public Fraction plus(Fraction augend) {
-            return new Fraction(293847);
+            return new Fraction(0);
         }
 
         public int intValue() {
-            return 0;
+            return integerValue;
         }
     }
 }
