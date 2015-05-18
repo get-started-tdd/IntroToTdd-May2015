c7468019c7d9c8628d81d5345ff18f6db8aaf8fa
J. B. Rainsberger <me@jbrains.ca>
2015-05-18 16:57:49 +0200

    We now have the opportunity to choose different prices based on the barcode.

--- test/source/ca/jbrains/pos/test/SellOneItemTest.java
+++ test/source/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,27 +1,52 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
-        final Sale sale = new Sale();
         final Display display = new Display();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("7,95 EUR", display.getText());
     }
 
+    @Test
+    @Ignore
+    public void anotherProductFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("12,50 EUR", display.getText());
+    }
+
     public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            display.setText("7,95 EUR");
         }
     }
-    
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "7,95 EUR";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
